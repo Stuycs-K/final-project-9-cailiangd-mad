90b396edb06289341d527bad4597cf6faf2365c5
David Cai Liang <dcailiang40@stuy.edu>
2023-05-22 14:30:32 -0400

    Created Circuit and Component classes.

---
+++ Component.java
@@ -0,0 +1,10 @@
+public class Component{
+  private double resistance;
+  private double current;
+  private double voltage;
+  private double power;
+  private ArrayList<Component> previous;
+  private ArrayList<Component> following;
+  private int x;
+  private int y;
+}
